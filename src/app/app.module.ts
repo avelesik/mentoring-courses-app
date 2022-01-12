92b4747ffdbd814750addf1741d6e2248569e4aa
hanna_kisialiova <avelesik007@gmail.com>
2022-01-12 22:44:52 +0300

    Home task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
+import { CoursesModule } from './features/courses/courses.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoursesModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
